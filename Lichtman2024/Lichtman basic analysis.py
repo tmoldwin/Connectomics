e9bf7cb1bc74c8d338231f2743f61896c5658513
Toviah Moldwin <tmoldwin@gmail.com>
2024-07-24 18:11:53 +0300

    Added basic analysis

--- Lichtman2024/Lichtman basic analysis.py
+++ Lichtman2024/Lichtman basic analysis.py
@@ -2,7 +2,8 @@ import pandas as pd
 
 #A. Read the csv file into a pandas object
 #B. Give counts per pre-type and post type
-#C. Filter the dataset by where the *post* type is a neuron of some sort
+#C.1. Create a dict of all unique pre and/or post IDs, mapped to type (e.g. pyramidal neuron, layer 4)
+    2. Find the IDs of things of things that are neurons
 #D. For each UID that is a neuron (either presynaptic or postsynaptic) create an object which contains 1. Neuron type, 2. Layer,
 # 3. a dataframe of its input synapses 4. a dataframe of its output synapses, also both 3 and 4, filtered by where the input/output is a neuron.
 ________
